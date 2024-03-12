d3c0777fccbad5713b6e4d9dbb7aa43e2eba21b2
Zdenka Sosnova <zdendasosnova@gmail.com>
2024-03-12 14:39:35 +0100

    Created one-to-many association - owner|dog

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,10 @@
 class Owner < ApplicationRecord
-    validates :name, presence: true, length: {minimum: 5, maximum: 25}, uniqueness: true
-    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "format is invalid" }
+    before_save { self.email = email.downcase}
+    before_save { self.name = name.titleize}
+    has_many :dogs
+    validates :name, presence: true, 
+            length: {minimum: 5, maximum: 25},
+            uniqueness: true
+    validates :email, presence: true, 
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "format is invalid" }
 end
